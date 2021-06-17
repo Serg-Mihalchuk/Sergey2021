6587d2774f13c491cbd86ebb1080af1303c10144
Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>
2021-06-17 13:57:16 +0300

    fist lesson

---
+++ Lesson1/src/Integer.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Integer {
+
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+        int num = 0;
+        System.out.println("Введитте 5 произвольных чисел:");
+        for (int i = 0; i < 5; i++) {
+            int Integer = console.nextInt();
+            if (Integer >= 0) {
+                num++;
+            }
+        }
+        System.out.println("Количество положительных чисел = " + num);
+    }
+
+
+}
+
