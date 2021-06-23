82b04d53b38c7a60624fbbee0853c0fe9c98eb01
Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>
2021-06-23 12:11:01 +0300

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>

--- Lesson2/src/Homework4.java
+++ Lesson2/src/Homework4.java
@@ -1,7 +1,7 @@
 public class Homework4 {
     public static void main(String[] args) {
-        int mass[] = {1, 2, 3, 4, 5, 100, 99};
-        int max = 0;
+        int mass[] = {-1, -2, -3, -4, -5, 0, 10};
+        int max = mass[0];
         for (int i = 0; i < mass.length; i++) {
             if (mass[i] > max) {
                 max = mass[i];
