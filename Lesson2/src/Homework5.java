ccea682a33028c04c36fac23e118682dd4ced079
Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>
2021-06-23 10:57:04 +0300

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>

---
+++ Lesson2/src/Homework5.java
@@ -0,0 +1,8 @@
+public class Homework5 {
+    public static void main(String[] args) {
+        double a = 3, b = 5;
+        double result;
+        result = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        System.out.println(result);
+    }
+}
