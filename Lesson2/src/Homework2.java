c4dee96d2d80df39b4b5b5d9c2f5c46f2a3b28d5
Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>
2021-06-23 10:33:47 +0300

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>

---
+++ Lesson2/src/Homework2.java
@@ -0,0 +1,12 @@
+public class Homework2 {
+    public static void main(String[] args) {
+        int a, b, max;
+        a = 15;
+        b = 9;
+        if (a > b) {
+            max = a;
+        } else max = b;
+        System.out.println(max);
+
+    }
+}
