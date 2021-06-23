257d76084b75e72b92255dd201a97fc0af189ba7
Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>
2021-06-23 10:29:29 +0300

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>

---
+++ Lesson2/src/Homework1.java
@@ -0,0 +1,9 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        int a, b, sum;
+        a = 4;
+        b = 5;
+        sum = a + b;
+        System.out.println("Сумма чисел =" + sum);
+    }
+}
