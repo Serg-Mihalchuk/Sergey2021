f202a1aec2765c30c0d43b4ac6756675d18d6039
Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>
2021-07-19 19:18:47 +0300

    Merge remote-tracking branch 'origin/main'
    Signed-off-by: Serg-Mihalchuk <80647769+Serg-Mihalchuk@users.noreply.github.com>

---
+++ Lesson5/Lesson5.1/src/main/java/com/home/servise/Main.java
@@ -0,0 +1,14 @@
+package com.home.servise;
+
+import com.home.modul.Computer;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Computer computer = new Computer("Intell", 8, 500, 5);
+        computer.info();
+
+    }
+
+}
