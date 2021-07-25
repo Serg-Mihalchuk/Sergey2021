9dbac70fdc8944b9478127770937d3586748b459
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2021-07-25 12:04:26 +0300

    добавление мана в лекцию 5.1

---
+++ Lesson5.1/src/main/java/com/home/service/Main.java
@@ -0,0 +1,10 @@
+package com.home.service;
+
+import com.home.modul.Computer;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer = new Computer("Intell", 8, 500, 5);
+        computer.info();
+    }
+}
